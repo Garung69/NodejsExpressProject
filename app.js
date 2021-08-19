b37884798dd452f806d98a47476cb98d5cae95d7
Garung69 <thienlmgch190355@fpt.edu.vn>
2021-08-19 11:52:25 +0700

    fixx zoom

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
+//express library declaration
 const EXPRESS = require('express')
-const { Int32, Double} = require('mongodb');
-
-const { registerAcc,login,getAllProduct,getOwnProduct,getUser, addNewToy,deleteProduct} = require('./script');
+//Declare the input data type
+const {Double} = require('mongodb');
+//declare functions from javascript files
+const { registerAcc,login,getAllProduct,getOwnProduct,getUser, addNewToy,deleteProduct, getLogServer,createLog,createLogAdmin, getLogAdminServer} = require('./script');
 
 const APP = EXPRESS()
 
+APP.use(EXPRESS.static('resource'))
 APP.use(EXPRESS.urlencoded({extended:true}))
 APP.set('view engine','hbs')
 
-
+//render home file when open default localhost
 APP.get('/',async (req,res)=>{
     res.render('home');
-
 })
+//Executed when receiving registration request
 APP.post('/register',async (req,res)=>{
+    //Receive input data
     const username = req.body.username;
     const password = req.body.password;
     const newAccount = {Username:username,Password: password};
+    //Execute add new data  
     await registerAcc(newAccount, username);
+    //Create log for admin manage
+    createLog(username, "register");
+    //render home file again
     res.redirect('/');
 })
+
+//Executed when receiving login request
 APP.post('/login',async (req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
+    //Get data form database
     const allProduct = await getAllProduct();
     const ownProduct = await getOwnProduct(username);
     const user = await getUser(username);
+    //if user login => render dashboard
     if((await login(username, password))== 2){
+        createLog(username, "login");
         res.render('dashboard', {productData: allProduct, ownProductData: ownProduct, User: user});
     }
+    //if admin login => render dashboard
+    if((await login(username, password))== 1){
+        const server = await getLogServer();
+        const adminServer = await getLogAdminServer();
+        res.render('admin', {server: server,productData: allProduct,adminServer: adminServer});
+    }
+    //if not found user => render home file again
     else res.render('home');
 })
+
+//Executed when receiving add product request
 APP.post('/addNewToy',async (req,res)=>{
     const toyName = req.body.newToyName;
     const toyImg = req.body.newToyImg;
@@ -40,24 +62,42 @@ APP.post('/addNewToy',async (req,res)=>{
     } else toyInfo = req.body.newToyInfo;
     const username = req.body.newToyOwner;
     const newProduct = {NameP:toyName,ImgP: toyImg,SellerP:username, PriceP: Double(toyPrice), InfoP: toyInfo};
+    //add new product
     await addNewToy(newProduct);
+    //create log
+    createLog(username, "add a product to shop");
+    //get data
     const user = await getUser(username);
     const allProduct = await getAllProduct();
     const ownProduct = await getOwnProduct(username);
     res.render('dashboard', {productData: allProduct, ownProductData: ownProduct, User: user});
 })
 
+//Executed when receiving delete product request
 APP.post('/delete',async (req,res)=>{
     const idInput = req.body.idP;
-    console.log("chay den day")
-    console.log(idInput)
     const username = req.body.ownP;
+    const role = req.body.role;
     await deleteProduct(idInput);
     const user = await getUser(username);
     const allProduct = await getAllProduct();
     const ownProduct = await getOwnProduct(username);
-    res.render('dashboard', {productData: allProduct, ownProductData: ownProduct, User: user});
+    //if admin delete product create logAdmin and render admin file
+    if(role == "admin"){
+        const server = await getLogServer();
+        const adminServer = await getLogAdminServer();
+        createLogAdmin(role, "deleted a "+ username +"'s product in shop");
+        res.render('admin', {server: server,productData: allProduct,adminServer: adminServer});
+    }
+    //if user unSell product create userLog and render dashboard file
+    else{
+        createLog(username, "deleted a product in shop");
+        res.render('dashboard', {productData: allProduct, ownProductData: ownProduct, User: user});
+    }
+    
 })
 
+//port declaration
 const PORT = process.env.PORT || 5000;
+//Ask the server to point to the port
 APP.listen(PORT);
--- script.js
+++ script.js
@@ -1,27 +1,29 @@
+//declare library mongodb
 const {MongoClient,ObjectId} = require('mongodb');
 
-//const URL = 'mongodb://localhost:27017';
+//declare database
 const URL = 'mongodb+srv://garung69:tavip123@cluster0.dvp5p.mongodb.net/test';
 const DATABASE_NAME = "GCH0803DB"
 
+//get data form database
 async function getDB() {
     const client = await MongoClient.connect(URL);
     const dbo = client.db(DATABASE_NAME);
     return dbo;
 }
 
-
+//function to register
 async function registerAcc(newAccount, username) {
     const dbo = await getDB();
     const Account = await dbo.collection("account").findOne({Username: username});
+    //Can only create a new account with a username that doesn't exist in the database.
     if(Account == undefined){
         const newS = await dbo.collection("account").insertOne(newAccount);
-        console.log("New account created with id: ", newS.insertedId.toHexString());
     }
     else console.log("Account already exists");
-
 }
 
+//function to login
 async function login(username, password){
     const dbo = await getDB();
     const Account = await dbo.collection("account").findOne({Username: username, Password: password});
@@ -31,31 +33,76 @@ async function login(username, password){
     }
     else return 2;
 }
+
+//function to get all product data
 async function getAllProduct(){
     const dbo = await getDB();
     const allProduct = await dbo.collection("product").find({}).toArray();
     return allProduct;
 }
+
+//function to get user product data
 async function getOwnProduct(username){
     const dbo = await getDB();
     const allProduct = await dbo.collection("product").find({SellerP: username}).toArray();
     return allProduct;
 }
+
+//function to get user data
 async function getUser(username){
     const dbo = await getDB();
     const User = await dbo.collection("account").find({Username: username}).toArray();
-    console.log(User);
     return User;
 }
+
+//function to add new product
 async function addNewToy(newProduct){
     const dbo = await getDB();
     const Added = await dbo.collection('product').insertOne(newProduct);
-    console.log("New account created with id: ", Added.insertedId.toHexString());
 }
 
+//function to delete a product
 async function deleteProduct(idInput) {
-    console.log(idInput)
     const dbo = await getDB();
     await dbo.collection("product").deleteOne({ _id: ObjectId(idInput) });
 }
-module.exports = {getDB,registerAcc,login,getAllProduct, getOwnProduct,getUser, addNewToy, deleteProduct}
+
+//function to get UserLog
+async function getLogServer(){
+    const dbo = await getDB();
+    const server = await dbo.collection("server").find({}).toArray();
+    return server;
+}
+
+//function to get AdminLog
+async function getLogAdminServer(){
+    const dbo = await getDB();
+    const server = await dbo.collection("server").find({Id: 1}).toArray();
+    return server;
+}
+
+//function to get currentDate
+function getCurrentDate(){
+    return new Date().toLocaleString("vi-VN");
+}
+
+//function to create AdminLog
+async function createLog(username,action){
+    const dbo = await getDB();
+    var log = "User: " +username+ " had " + action + " at " + getCurrentDate();
+    console.log(log);
+    const newLog = {ServerLog: log}
+    await dbo.collection("server").insertOne(newLog);
+}
+
+//function to create userLog
+async function createLogAdmin(username, act){
+    const dbo = await getDB();
+    var log = username+ " had " + act + " at " + getCurrentDate();
+    var id = id1;
+    console.log(log);
+    const newLog = {ServerLog: log,Id: id}
+    await dbo.collection("server").insertOne(newLog);
+}
+//export functions
+module.exports = {getDB,registerAcc,login,getAllProduct, getOwnProduct,getUser, addNewToy, deleteProduct, createLog, createLogAdmin, getLogServer, getLogAdminServer}
